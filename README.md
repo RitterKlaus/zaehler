c83cc893e38bdeabf09a2fe69f38b4bbbb52012f
RitterKlaus <48477737+RitterKlaus@users.noreply.github.com>
2022-09-25 19:25:34 +0200

    Statistik und Website hinzugefügt

--- README.md
+++ README.md
@@ -16,3 +16,20 @@ Deployment:
 Secure copy (http://www.hypexr.org/linux_scp_help.php)
 
 scp interrupt.py pi@192.168.178.52:/home/pi/dev/
+
+
+# Abläufe
+1. Pi protokolliert jede Zählerumdrehung durch den Magnetkontakt mit sekundengenauem Zeitstempel.
+1. Jede Nacht werden aus den gesammelten Daten zwei Statistiken erzeugt: Gasverbrauch pro Tag und Gasverbrauch pro Stunde des Tages.
+1. Diese Bilder und ein Nackup der gezippten Protokolldatei werden auf die Website hochgeladen.
+1. Die Inhalte der Website liegen im Ordner web/
+
+
+# Installation
+
+* Verzeichnis web/ auf geeigneter Website hochladen
+* interrupt.py und create_report_py auf dem Raspberry kopieren
+* interrupt.py 'headless' starten
+* Konfigdaten (u.a. FTP-Passwort) in create_report.py eintragen
+* cron-Job für Start von create_report.py einrichten
+
--- create_report.py
+++ create_report.py
@@ -2,30 +2,51 @@ from datetime import datetime
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('zaehler-20220908.csv', sep=';', names=['Impuls', 'Nummer', 'Label', 'Zeitpunkt'])
-#df.set_index('Date',inplace=True)
-
-#with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
-#    print(df)
-
+# Hilfreiche Befehle
 # print(df.to_string())
+# print(df.dtypes)
+# print (df_gruppiert.dtypes)
+# print(df_gruppiert.to_string())
+# plt.show()
 
-print (df.dtypes)
+############################# Daten vorbereiten ###############################
+# CSV-Datei in Pandas einlesen, Spaltentitel definieren
+df = pd.read_csv('zaehler-30.csv', sep=';', names=['Impuls', 'Nummer', 'Label', 'Zeitpunkt'])
+#df = pd.read_csv('zaehler-20220908.csv', sep=';', names=['Impuls', 'Nummer', 'Label', 'Zeitpunkt'])#
 
+# unnötige Spalten löschen
 df = df.drop(columns=['Impuls', 'Nummer', 'Label'])
+
+# Zeitpunkt-Spalte in das richtige Format bringen
 df['Zeitpunkt'] = pd.to_datetime(df['Zeitpunkt'], format='%Y-%m-%d %H:%M:%S.%f')
 df['Datum'] = pd.to_datetime(df['Zeitpunkt'], format='%Y-%m-%d')
 
-print (df.dtypes)
+############################# Summe nach Datum (Tag) ###############################
+# Summe pro Tag bilden (gruppieren)
+##df_gruppiert_datum = df.groupby(pd.Grouper(key='Datum',freq='5D')).agg(summe=('Datum', 'count'))
 
-#df['Date'] = pd.to_datetime(df['timestamp']).dt.date
+# Diagramm erstellen
+##gas_nach_datum = df_gruppiert_datum.plot(kind = 'bar', xlabel = 'Datum', ylabel = 'Zählimpulse', legend = False)
 
-df_gruppiert = df.groupby(pd.Grouper(key='Datum',freq='5D')).agg(summe=('Datum', 'count'))
+# Beschriftung der x-Achse in ein schönes Format bringen
+##x_labels = df_gruppiert_datum.index.strftime('%d.%m.')
+##gas_nach_datum.set_xticklabels(x_labels)
 
-print (df_gruppiert.dtypes)
-print(df_gruppiert.to_string())
+# Ergebnis als Bild speichern
+##plt.savefig('diagramm_gas_pro_tag.png')
 
+############################# Summe nach Tageszeit ###############################
+# Summe pro Tag bilden (gruppieren)
 
-df_gruppiert.plot(kind = 'bar')
-#plt.show()
-plt.savefig('bild.png')
+df_gruppiert_tageszeit = df.groupby(df['Zeitpunkt'].dt.hour).count()
+# unnötige Spalten löschen
+df_gruppiert_tageszeit = df_gruppiert_tageszeit.drop(columns=['Datum'])
+
+print(df_gruppiert_tageszeit.to_string())
+print(df_gruppiert_tageszeit.dtypes)
+
+# Diagramm erstellen
+gas_nach_tageszeit = df_gruppiert_tageszeit.plot(kind = 'bar', xlabel = 'Stunde', ylabel = 'Zählimpulse', legend = False)
+
+# Ergebnis als Bild speichern
+plt.savefig('diagramm_gas_pro_stunde.png')
